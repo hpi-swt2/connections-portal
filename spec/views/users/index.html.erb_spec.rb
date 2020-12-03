d1df83b36b7a0cba469c04af9494c88c31ffb175
Paula <paula.klinke@student.hpi.de>
2020-12-03 14:20:32 +0100

    Failing test for #25

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -11,4 +11,9 @@ RSpec.describe "users/index", type: :view do
       expect(rendered).to match user.email
     end
   end
+
+  it "has an + button to add a contact" do
+    render
+    expect(rendered).to have_text('+', count: @users.length)
+  end
 end
