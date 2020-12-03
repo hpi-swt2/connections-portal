ab1f5a79a045f33895a00f9b97502b5073947565
sandro <sandro.speh@student.hpi.de>
2020-12-03 16:11:56 +0100

    fixed consequences of first implementation

--- app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < ApplicationController
-  # GET /users
-  def index
-    @users = User.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
   resources :notes
-  resources :users
   # https://github.com/heartcombo/devise/wiki/
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
