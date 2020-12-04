40133bddf3817fa073b76e04ff720098a84200fa
Christoph Matthies <chrisma@users.noreply.github.com>
2020-12-04 14:38:35 +0100

    Include Devise test helpers, sign_in user in view test

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "home/index", type: :view do
   before(:each) do
     @users = assign(:users, FactoryBot.create_list(:user, 3))
+    sign_in @users.first
   end
 
   it "renders a list of users" do
