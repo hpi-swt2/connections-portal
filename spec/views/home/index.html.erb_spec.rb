4f8c63588002f9a5b67c5ebb510b933c6e4a073e
Patricia <patricia.sowa@student.hpi.de>
2020-12-04 14:20:10 +0100

    Add a test for homepage

---
+++ spec/views/home/index.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "home/index", type: :view do
+  before(:each) do
+    @users = assign(:users, FactoryBot.create_list(:user, 3))
+  end
+
+  it "renders a list of users" do
+    render
+    @users.each do |user|
+      expect(rendered).to match user.email
+    end
+  end
+end
