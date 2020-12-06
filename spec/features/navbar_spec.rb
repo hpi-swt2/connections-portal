5616e0df1c75bf2a746d7ef6f4a406f35097d76f
konradh <konrad.hanff@student.hpi.de>
2020-12-06 17:47:23 +0100

    Improve tests for #34

--- spec/features/navbar_spec.rb
+++ spec/features/navbar_spec.rb
@@ -15,20 +15,24 @@ RSpec.describe 'Navbar', driver: :selenium_headless, type: :feature, js: true do
       expect(profile_dropdown['class']).to_not include('show')
     end
 
-    it 'is expands after being clicked on' do
+    it 'expands after being clicked on' do
       toggle_profile_dropdown
-
       profile_dropdown = page.find('#navbarProfileDropdown + div')
       expect(profile_dropdown['class']).to include('show')
     end
 
     it 'contains a link to the users profile page' do
-
+      toggle_profile_dropdown
+      within '#navbarProfileDropdown + div' do
+        expect(page).to have_link(href: user_path(@user))
+      end
     end
 
     it 'contains a link to the users edit profile page' do
       toggle_profile_dropdown
-      expect(page).to have_link('Edit my profile', href: edit_user_registration_path(@user))
+      within '#navbarProfileDropdown + div' do
+        expect(page).to have_link(href: edit_user_registration_path(@user))
+      end
     end
 
     def toggle_profile_dropdown
