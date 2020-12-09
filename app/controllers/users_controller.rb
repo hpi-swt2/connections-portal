cab210bfa16a631b81e45bf83134c902daa84b65
jostafarr <jost.goette@student.hpi.de>
2020-12-09 20:30:21 +0100

    Add functionality to the add contact button and test it.
    Co-authored-by: Paula-Kli <Paula-Kli@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   # GET /users
+
   def index
     @users = User.all
   end
+
+  def add_contact
+    current_user.contacts << User.find(params[:id])
+    current_user.save
+    redirect_to home_index_path
+  end
+
 end
