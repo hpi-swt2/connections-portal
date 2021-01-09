b8fd7987e8169c9d77b0260c2a9e7a0348cb7163
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2021-01-09 14:37:09 +0100

    Implement user filtering by status

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,19 @@
 class HomeController < ApplicationController
+  before_action :filter_users_status
 
   def dashboard
-    @users = User.all
     if user_signed_in?
       render 'dashboard'
     else
       render 'index'
     end
+  end
+
+  def filter_users_status
+    @status_filter = User.default_status_filter
+    @status_filter = params[:filter_status] if User.filter_status_list.include?(params[:filter_status])
+
+    @users = User.with_status(@status_filter)
+    @users = @users.shuffle
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
                           foreign_key: 'requested_user_id',
                           association_foreign_key: 'requesting_user_id'
 
-  VALID_STATUS_LIST = %w[available working free_for_chat].freeze
+  VALID_STATUS_LIST = %w[available working free_for_chat offline].freeze
 
   validates :username, :email, presence: true
   validates :current_status, inclusion: { in: VALID_STATUS_LIST }
@@ -28,15 +28,30 @@ class User < ApplicationRecord
 
   attribute :current_status, :string, default: 'available'
 
+  scope :with_status, ->(status) { where(current_status: status) }
+
   def init
     self.username ||= email.split('@', 2)[0]
   end
 
   def notes
-    Note.where('creator_user_id = ?', self.id)
+    Note.where('creator_user_id = ?', id)
   end
 
-  def select_status_list
+  # class methods
+  def self.select_status_list
     VALID_STATUS_LIST.map { |status| [I18n.t("user.status.#{status}"), status] }
+  end
+
+  def self.filter_status_list
+    VALID_STATUS_LIST.reject { |status| status == 'offline' }
+  end
+
+  def self.select_filter_status_list
+    filter_status_list.map { |status| [I18n.t("user.status.#{status}"), status] }
+  end
+
+  def self.default_status_filter
+    'free_for_chat'
   end
 end
