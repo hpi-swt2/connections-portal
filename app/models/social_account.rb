97a5ecb73903a9d16e15bf0f775af02f93ec45fb
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2021-01-13 15:54:57 +0100

    Simplify validation

--- app/models/social_account.rb
+++ app/models/social_account.rb
@@ -1,4 +1,4 @@
 class SocialAccount < ApplicationRecord
-    validates :social_network, presence: true, allow_blank: false
-    validates :user_name, presence: true, allow_blank: false
+    validates :social_network, presence: true
+    validates :user_name, presence: true
 end
