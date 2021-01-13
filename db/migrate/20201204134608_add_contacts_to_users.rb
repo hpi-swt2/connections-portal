86a60645a4f0b985090402f811ad39e6bd7ad038
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2021-01-13 17:03:15 +0100

    Disable cop in migration that is running in production

--- db/migrate/20201204134608_add_contacts_to_users.rb
+++ db/migrate/20201204134608_add_contacts_to_users.rb
@@ -1,8 +1,11 @@
 class AddContactsToUsers < ActiveRecord::Migration[6.0]
+  # This migration runs already in production so I do not want to tamper with it
+  # rubocop:disable Rails/CreateTableWithTimestamps
   def change
     create_table :users_users, id: false do |t|
       t.belongs_to :user
       t.belongs_to :contact
     end
   end
+  # rubocop:enable Rails/CreateTableWithTimestamps
 end
