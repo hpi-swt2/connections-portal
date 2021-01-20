4f51a3d707b8722141f5321a9d4527503ca5de0e
Paula <paula.klinke@student.hpi.de>
2021-01-20 21:27:51 +0100

    Rename test#

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 describe 'Filter users by key', type: :feature do
   let(:users) { FactoryBot.create_list(:user, 3) }
-  let(:user) { FactoryBot.create :user }
+  let(:signed_in_user) { FactoryBot.create :user }
 
   before do
     users
-    sign_in user
+    sign_in signed_in_user
     visit users_path
   end
 
-  it 'is possible to add all users except one' do
+  it 'is possible to add all other users' do
     expect(page).to have_button('+', count: users.length)
   end
 
   it 'is not possible to see myself in the users list' do
-    expect(page).not_to have_text(user.email)
+    expect(page).not_to have_text(signed_in_user.email)
   end
 end
