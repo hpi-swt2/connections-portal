97a2ab7d6e3862184cbd468ad25c2e7bdfcf9f28
Sandro <sandro.speh@student.hpi.de>
2021-01-21 10:19:53 +0100

    refactored feature test
    Co-authored-by: Paula-Kli <Paula-Kli@users.noreply.github.com>

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -7,14 +7,41 @@ describe 'Filter users by key', type: :feature do
   before do
     users
     sign_in signed_in_user
+  end
+
+  context 'with a user that has no contacts and contact requests send yet' do
+    before do
+      visit users_path
+    end
+
+    it 'is possible to add all other users' do
+      expect(page).to have_button('+', count: users.length)
+    end
+
+    it 'is not possible to see myself in the users list' do
+      expect(page).not_to have_text(signed_in_user.email)
+    end
+
+    it 'does have a + button for not yet requested user' do
+      users.each do |user|
+        expect(page).to have_css("form[action='#{user_contact_requests_path(user)}']")
+      end
+    end
+  end
+
+  it 'does not have a + button for already sent contact request' do
+    users.second.contact_requests << signed_in_user
     visit users_path
+    expect(page).not_to have_css("form[action='#{user_contact_requests_path(users.second)}']")
   end
 
-  it 'is possible to add all other users' do
-    expect(page).to have_button('+', count: users.length)
+  it 'does not have a + button for already existing contacts' do
+    signed_in_user.contacts << users.second
+    visit users_path
+    expect(page).to have_css("form[action='#{user_contact_requests_path(users.first)}']")
+    expect(page).not_to have_css("form[action='#{user_contact_requests_path(users.second)}']")
   end
 
-  it 'is not possible to see myself in the users list' do
-    expect(page).not_to have_text(signed_in_user.email)
-  end
 end
+
+
