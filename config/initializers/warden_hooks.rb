370d9addb914e73af8f10784db99af224ae6de9c
sirkrypt0 <tobiaskantusch@online.de>
2021-01-22 10:23:53 +0100

    Add comment on warden_hooks #77

--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -1,3 +1,7 @@
+# When establishing a websocket connection, we don’t have access to the user session
+# but we do have access to the cookies. In order to authenticate the user we need to
+# do some devise related stuff first.
+
 Warden::Manager.after_set_user do |user, auth, opts|
   scope = opts[:scope]
   auth.cookies.signed["#{scope}.id"] = user.id
