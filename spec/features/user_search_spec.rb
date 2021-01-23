b362e4382f68dc36ae1f8da4437c648226bafe12
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2021-01-23 12:01:01 +0100

    Rework flaky tests in user search spec

--- spec/features/user_search_spec.rb
+++ spec/features/user_search_spec.rb
@@ -2,50 +2,75 @@ require 'rails_helper'
 
 describe 'Filter users by key', type: :feature do
   let(:user) { FactoryBot.create :user }
-  let(:second_user) { FactoryBot.create :user }
 
-  before do
-    second_user
-    sign_in user
+  before { sign_in user }
+
+  describe 'without filter' do
+    let!(:users) { FactoryBot.create_list(:user, 3) }
+
+    before { visit search_users_path }
+
+    it 'does show all other users when no filter is given' do
+      users.each { |user| expect(page).to have_text(user.email) }
+    end
+
+    it 'does not show myself' do
+      expect(page).not_to have_text(user.email)
+    end
   end
 
-  it 'is possible to filter by firstname' do
-    visit search_users_path(search: second_user.firstname[0..2])
-    expect(page).to have_text(second_user.firstname)
-  end
+  describe 'with filter' do
+    let!(:erika) { FactoryBot.create :user, :erika }
+    let!(:maximilian) { FactoryBot.create :user, :maximilian }
 
-  it 'is possible to filter by lastname' do
-    visit search_users_path(search: second_user.lastname[0..2])
-    expect(page).to have_text(second_user.lastname)
-  end
+    before { visit search_users_path(search: search) }
 
-  it 'is possible to filter by username' do
-    visit search_users_path(search: second_user.username[0..2])
-    expect(page).to have_text(second_user.username)
-  end
+    context 'with part of firstname' do
+      let(:search) { 'aximi' }
 
-  it 'is possible to filter by email' do
-    visit search_users_path(search: second_user.email[0..2])
-    expect(page).to have_text(second_user.email)
-  end
+      it 'is possible to filter by firstname' do
+        expect(page).to have_text(maximilian.email)
+      end
 
-  it 'does not show non-matching users when filtering by firstname' do
-    visit search_users_path(search: user.firstname[0..2])
-    expect(page).not_to have_text(second_user.firstname)
-  end
+      it 'does not show non-matching users when filtering by firstname' do
+        expect(page).not_to have_text(erika.email)
+      end
+    end
 
-  it 'does not show non-matching users when filtering by lastname' do
-    visit search_users_path(search: user.lastname[0..2])
-    expect(page).not_to have_text(second_user.lastname)
-  end
+    context 'with part of lastname' do
+      let(:search) { 'erma' }
 
-  it 'does not show non-matching users when filtering by username' do
-    visit search_users_path(search: user.username[0..2])
-    expect(page).not_to have_text(second_user.username)
-  end
+      it 'is possible to filter by lastname' do
+        expect(page).to have_text(maximilian.email)
+      end
 
-  it 'does not show non-matching users when filtering by email' do
-    visit search_users_path(search: user.email[0..2])
-    expect(page).not_to have_text(second_user.email)
+      it 'does not show non-matching users when filtering by lastname' do
+        expect(page).not_to have_text(erika.email)
+      end
+    end
+
+    context 'with part of username' do
+      let(:search) { 'andard_' }
+
+      it 'is possible to filter by username' do
+        expect(page).to have_text(maximilian.email)
+      end
+
+      it 'does not show non-matching users when filtering by username' do
+        expect(page).not_to have_text(erika.email)
+      end
+    end
+
+    context 'with part of email' do
+      let(:search) { 'test@exa' }
+
+      it 'is possible to filter by email' do
+        expect(page).to have_text(maximilian.email)
+      end
+
+      it 'does not show non-matching users when filtering by email' do
+        expect(page).not_to have_text(erika.email)
+      end
+    end
   end
 end
