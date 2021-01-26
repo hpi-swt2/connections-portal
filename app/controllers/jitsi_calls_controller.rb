49cad28c18b1421fb6dd401ca4f6292d12c29a2d
frcroth <frcroth@gmail.com>
2021-01-26 12:25:17 +0100

    Address review comments, rename user_id to participant_id in jitsi call create

--- app/controllers/jitsi_calls_controller.rb
+++ app/controllers/jitsi_calls_controller.rb
@@ -3,20 +3,21 @@ class JitsiCallsController < ApplicationController
 
   def create
     room_name = SecureRandom.uuid
-    @jitsi_call = JitsiCall.new({ room_name: room_name })
-    if @jitsi_call.save && User.exists?(call_params[:user_id])
+    @jitsi_call = JitsiCall.new(room_name: room_name)
+    if @jitsi_call.save && User.exists?(call_params[:participant_id])
       assign_participants
-    else redirect_to root_path, alert: I18n.t('denial.resource_creation', resource: JitsiCall) end
+    else redirect_to root_path, alert: I18n.t('denial.resource_creation', resource: JitsiCall)
+    end
   end
 
   def assign_participants
-    @jitsi_call.call_participants.create({ state: 'accepted', role: 'initiator', user: current_user })
-    @jitsi_call.call_participants.create({ state: 'requested',
-                                           role: 'participant',
-                                           user: User.find(call_params[:user_id]) })
+    @jitsi_call.call_participants.create(state: 'accepted', role: 'initiator', user: current_user)
+    @jitsi_call.call_participants.create(state: 'requested',
+                                         role: 'participant',
+                                         user: User.find(call_params[:participant_id]))
   end
 
   def call_params
-    params.require(:jitsi_call).permit(:user_id)
+    params.require(:jitsi_call).permit(:participant_id)
   end
 end
--- spec/requests/jitsi_call_request_spec.rb
+++ spec/requests/jitsi_call_request_spec.rb
@@ -5,24 +5,22 @@ RSpec.describe 'Jitsi Call', type: :request do
     context 'when logged in' do
       let(:participant) { FactoryBot.create(:user) }
 
-      before do
-        sign_in FactoryBot.create(:user)
-      end
+      before { sign_in FactoryBot.create(:user) }
 
       it 'creates a new call' do
-        post jitsi_calls_path, params: { jitsi_call: { user_id: participant.id } }
+        post jitsi_calls_path, params: { jitsi_call: { participant_id: participant.id } }
         expect(response).to have_http_status(:no_content)
       end
 
       it 'does not create a new call without participant user id' do
-        post jitsi_calls_path, params: { jitsi_call: { user_id: nil } }
+        post jitsi_calls_path, params: { jitsi_call: { participant_id: nil } }
         expect(response).to redirect_to(root_path)
       end
     end
 
     context 'without logging in' do
       it 'redirects to login page' do
-        post jitsi_calls_path, params: { jitsi_call: { user_id: 0 } }
+        post jitsi_calls_path, params: { jitsi_call: { participant_id: 0 } }
         expect(response).to redirect_to(new_user_session_path)
       end
     end
