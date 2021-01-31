f9dbfa74653075ebc260d0ffc7363f45535db892
Patricia <patricia.sowa@student.hpi.de>
2021-01-31 12:46:20 +0100

    Improve sketches of possible tests for profile picture

--- spec/features/user_edit_profile_picture_spec.rb
+++ spec/features/user_edit_profile_picture_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe 'Profile picture', type: :feature do
     visit edit_user_path(user)
     find('input[type="file_field"]').click
     # here we have to upload a picture larger 5 MB
+    # something like that? attach_file 'Image', '/path/to/image.jpg'
     expect(page).to have_text("Avatar File size should be less than 5 MB")
   end
 
@@ -26,8 +27,17 @@ RSpec.describe 'Profile picture', type: :feature do
     visit edit_user_path(user)
     find('input[type="file_field"]').click
     # Here we have to upload a picture of non-valid type
+    # something like that? attach_file 'Image', '/path/to/image.pdf'
     expect(page).to have_text("Avatar is not a valid file format")
   end
+
+  it 'is a profile-picture shown on the user-show-page' do
+    sign_in user
+    visit user_path(user)
+    expect(page).to have_selector('image_tag')
+  end
+
+  # todo: model and controller tests
 end
 =end
 
