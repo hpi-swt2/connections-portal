043dbbea752d141de5a9e9da491e05a0b999fe0c
Sandro <sandro.speh@student.hpi.de>
2021-02-02 14:42:33 +0100

    created tests for avatar
    Co-authored-by: Jostafarr <jost.goette@student.hpi.de>
    Co-authored-by: PatriciaSowa <patricia.sowa@student.hpi.de>

--- spec/features/user_edit_profile_picture_spec.rb
+++ spec/features/user_edit_profile_picture_spec.rb
@@ -1,43 +1,46 @@
-=begin
 require 'rails_helper'
 
 RSpec.describe 'Profile picture', type: :feature do
 
   let(:user) { FactoryBot.create :user }
+  let(:contact) { FactoryBot.create :user }
+
 
   it 'is possible to upload a profile picture' do
     sign_in user
     visit edit_user_path(user)
-    find('input[type="file_field"]').click
-    # here we have to upload a normal picture e.g 'test_picture.png'
-    expect(page).to have_text("test_picture.png")
+    attach_file 'user_avatar', 'spec/support/assets/new-avatar.jpeg'
+    click_button 'Update User'
+    visit user_path(user)
+    expect(page).to have_css("img[src*='new-avatar.jpeg']")
   end
 
   it 'is not possible to upload a picture larger then 5 MB' do
     sign_in user
     visit edit_user_path(user)
-    find('input[type="file_field"]').click
-    # here we have to upload a picture larger 5 MB
-    # something like that? attach_file 'Image', '/path/to/image.jpg'
-    expect(page).to have_text("Avatar File size should be less than 5 MB")
+    attach_file 'user_avatar', 'spec/support/assets/avatar-big.jpeg'
+    click_button 'Update User'
+    expect(page).to have_text('Avatar File size should be less than 5 MB')
   end
 
-  it 'is only possible to upload pictures of type jpeg, jpg and png' do
+  it 'is only possible to upload pictures' do
     sign_in user
     visit edit_user_path(user)
-    find('input[type="file_field"]').click
-    # Here we have to upload a picture of non-valid type
-    # something like that? attach_file 'Image', '/path/to/image.pdf'
-    expect(page).to have_text("Avatar is not a valid file format")
+    attach_file 'user_avatar', 'spec/support/assets/avatar.mp4'
+    click_button 'Update User'
+    expect(page).to have_text('Avatar is not a valid file format')
   end
 
   it 'is a profile-picture shown on the user-show-page' do
     sign_in user
     visit user_path(user)
-    expect(page).to have_selector('image_tag')
+    expect(page).to have_css("img[src*='avatar-default-test.png']")
   end
 
-  # todo: model and controller tests
+  it 'is a profile-picture shown on the my-contacts-page' do
+    sign_in user
+    user.contacts << contact
+    visit user_contacts_path(user)
+    expect(page).to have_css("img[src*='avatar-default-test.png']")
+  end
 end
-=end
-
