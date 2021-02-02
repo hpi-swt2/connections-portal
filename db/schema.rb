a71c6f52c898a719173e2add236c6377edb0f082
jostafarr <jost.goette@student.hpi.de>
2021-02-02 14:26:43 +0100

    add active storage migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_133601) do
+ActiveRecord::Schema.define(version: 2021_02_02_132517) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_133601) do
     t.index ["user_id"], name: "index_users_users_on_user_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "activities", "users"
   add_foreign_key "notes", "users"
   add_foreign_key "notes", "users", column: "creator_user_id"
