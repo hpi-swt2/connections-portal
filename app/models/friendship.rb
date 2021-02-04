e4d0ad47b5741fcbf799110304c3c07063892e57
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2021-02-04 21:30:31 +0100

    Add more tests for friendships and fix some newly discovered bugs

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,14 +1,19 @@
 # A class for handling contacts which ensures that the association is always symmetrical
 class Friendship < ApplicationRecord
   self.table_name = 'users_users'
-  belongs_to :user, foreign_key: :contact_id, inverse_of: :friendships
+  self.primary_keys = :user_id, :contact_id
+
+  belongs_to :user, inverse_of: :friendships
+  belongs_to :contact, class_name: 'User', inverse_of: :friendships
 
   after_create do |c|
-    Friendship.create!(user_id: c.contact_id, contact_id: c.user_id) unless Friendship.find_by(contact_id: c.user_id)
+    unless Friendship.find_by(user_id: c.contact_id, contact_id: c.user_id)
+      Friendship.create(user_id: c.contact_id, contact_id: c.user_id)
+    end
   end
 
   after_destroy do |c|
-    reciprocal = Friendship.find_by(contact_id: c.user_id)
+    reciprocal = Friendship.find_by(contact_id: c.user_id, user_id: c.contact_id)
     reciprocal&.destroy
   end
 end
