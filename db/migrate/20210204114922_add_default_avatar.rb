3d5627e1b97072d6c6919c2bda19e5fe20ff2af9
Sandro <sandro.speh@student.hpi.de>
2021-02-04 12:56:56 +0100

    fix for avatar in migration

---
+++ db/migrate/20210204114922_add_default_avatar.rb
@@ -0,0 +1,13 @@
+class AddDefaultAvatar < ActiveRecord::Migration[6.0]
+  def up
+    User.all.each do |user|
+      next unless user.avatar.attached?
+
+      user.avatar.attach(
+        io: File.open(
+          Rails.root.join('app/assets/images/default_avatar.png')
+        ), filename: 'default_avatar.png', content_type: 'image/png'
+      )
+    end
+  end
+end
