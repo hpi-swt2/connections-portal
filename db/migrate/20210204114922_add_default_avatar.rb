3bef2e4b93b413bec1b50f7df712fd01eaaee066
Sandro <sandro.speh@student.hpi.de>
2021-02-04 13:12:49 +0100

    fixed for real now

--- db/migrate/20210204114922_add_default_avatar.rb
+++ db/migrate/20210204114922_add_default_avatar.rb
@@ -1,13 +1,14 @@
 class AddDefaultAvatar < ActiveRecord::Migration[6.0]
   def up
     User.all.each do |user|
-      next unless user.avatar.attached?
+      next if user.avatar.attached?
 
       user.avatar.attach(
         io: File.open(
           Rails.root.join('app/assets/images/default_avatar.png')
         ), filename: 'default_avatar.png', content_type: 'image/png'
       )
+      user.save
     end
   end
 end
