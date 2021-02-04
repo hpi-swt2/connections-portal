ebab277539d7fdd15bd0cf0cc4e17e03c87627d4
Paula <paula.klinke@student.hpi.de>
2021-02-04 15:05:37 +0100

    Fix linting and inverse_of issues

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,16 +1,14 @@
 # A class for handling contacts, when one contact is created the other way is too
 class Friendship < ApplicationRecord
   self.table_name = 'users_users'
-  belongs_to :user, foreign_key: :contact_id, inverse_of: :friendship
+  belongs_to :user, foreign_key: :contact_id, inverse_of: :friendships
 
   after_create do |c|
-    unless Friendship.find_by(contact_id: c.user_id)
-      Friendship.create!(user_id: c.contact_id, contact_id: c.user_id)
-    end
+    Friendship.create!(user_id: c.contact_id, contact_id: c.user_id) unless Friendship.find_by(contact_id: c.user_id)
   end
 
-  after_destroy do |c|
-    reciprocal = Friendship.find_by(contact_id: c.user_id)
-    reciprocal&.destroy
-  end
+after_destroy do |c|
+  reciprocal = Friendship.find_by(contact_id: c.user_id)
+  reciprocal&.destroy
+end
 end
