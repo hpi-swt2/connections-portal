61712e3859e3dfe54fcfd80c0eb15173f1193e35
Christoph Thiede <christoph.thiede@student.hpi.de>
2021-02-05 12:45:53 +0100

    todo comment

--- app/models/social_account.rb
+++ app/models/social_account.rb
@@ -3,3 +3,6 @@ class SocialAccount < ApplicationRecord
   validates :social_network, presence: true
   validates :user_name, presence: true
 end
+
+NEXT: Write logic and test for filter that only returns visible_social_accounts! Then use it in view ...
+We will also need to patch DB.
