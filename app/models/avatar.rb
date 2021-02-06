f176146e25814c26ee7077b5ce27145bcd5c9b1d
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2021-02-06 23:19:37 +0100

    Add avatar model with database table

---
+++ app/models/avatar.rb
@@ -0,0 +1,11 @@
+# A model for storing user avatars in the database
+class Avatar < ApplicationRecord
+  VALID_MIME_TYPES = %w[image/png image/jpg image/jpeg].freeze
+  MAX_FILE_SIZE_MEGABYTES = 5
+
+  validates :filename, :filesize, :file, :mime_type, presence: true
+  validates :mime_type, inclusion: VALID_MIME_TYPES
+  validates :filesize, numericality: { only_integer: true, less_than_or_equal_to: MAX_FILE_SIZE_MEGABYTES.megabytes }
+
+  belongs_to :user
+end
