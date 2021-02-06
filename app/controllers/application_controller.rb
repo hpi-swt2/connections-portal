542c77ab4dac3b48ee3d1979fe4cca3440a0dd58
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2021-02-06 23:20:19 +0100

    Remove avatar parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:avatar, :email, :password, :password_confirmation, :current_password)
+      u.permit(:email, :password, :password_confirmation, :current_password)
     end
   end
 
